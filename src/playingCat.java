2edb67bba0a7ae715650998f79c2ec4d8cde75e5
nishadchittangadan <59539337+nishadchittangadan@users.noreply.github.com>
2021-06-16 22:02:28 +0300

    Initial commit

---
+++ src/playingCat.java
@@ -0,0 +1,22 @@
+public class playingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer == true) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (temperature >= 25 && temperature <= 35) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true,27));
+    }
+
+}
